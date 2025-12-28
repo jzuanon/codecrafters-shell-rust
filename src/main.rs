5d96dd9c3248c19360f24fde7ed55ff94f48faf5
Jorge Zuanon <zuanon.jorge@gmail.com>
2025-12-28 11:20:39 -0300

    Implement echo

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,19 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-        if command.trim() == "exit" {
-            break;
+        let parts: Vec<&str> = command.split_whitespace().collect();    
+        match parts.get(0){
+            Some(&"echo") => {
+                println!("{}", &parts[1..].join(" "));
+            },  
+            Some(&"exit") => { 
+                break; 
+            },
+            _ => {
+                println!("{}: command not found", command.trim());
+            }
         }
-
-        println!("{}: command not found", command.trim());
+        
     }
     
 
