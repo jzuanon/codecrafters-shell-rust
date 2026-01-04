915fa64e6f10b4af50046a7a82aedfa1052f108f
Jorge Zuanon <zuanon.jorge@gmail.com>
2026-01-03 23:40:56 -0300

    Locate executable files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use std::{env};
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-
     let builtins = ["echo", "exit", "type"];
 
     loop {
@@ -12,29 +12,47 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-        let parts: Vec<&str> = command.split_whitespace().collect();    
-        match parts.get(0){
+        let parts: Vec<&str> = command.split_whitespace().collect();
+        match parts.get(0) {
             Some(&"echo") => {
                 println!("{}", &parts[1..].join(" "));
-            },  
-            Some(&"exit") => { 
-                break; 
-            },
+            }
+            Some(&"exit") => {
+                break;
+            }
             Some(&"type") => {
                 if builtins.contains(&parts[1..].join(" ").as_str()) {
                     println!("{} is a shell builtin", &parts[1..].join(" "));
-                }
-                else {
-                    println!("{}: not found", &parts[1..].join(" "));
-                }
+                } else {
+                    if let Some(path) = env::var_os("PATH") {
+                        let mut found = false;
+                        for dir in env::split_paths(&path) {
+                            if dir.join(&parts[1..].join(" ")).with_extension("").with_extension("exe").is_file() {
 
-            },
+                                #[cfg(target_os = "windows")]
+                                println!("{}", dir.join(&parts[1..].join(" ")).with_extension("exe").display());
+                                found = true;
+                                break;
+                                                               
+                                #[cfg(target_os = "linux")]
+                                let metadata = fs::metadata(dir.join(&parts[1..].join(" ")))?;
+                                #[cfg(target_os = "linux")]
+                                if metadata.permissions() & 0o111 != 0 {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if !found  {
+                            println!("{}: not found", &parts[1..].join(" "));
+                        }
+                    }
+                }
+            }
             _ => {
                 println!("{}: command not found", command.trim());
             }
         }
-        
     }
-    
-
 }
